5ba01e0fe8e6fbbc94a6c90162cfb264ef624706
zhou-efr <killian.zhou@efrei.net>
2023-04-23 00:18:50 +0200

    fix nvim

--- PimpMyParrot.sh
+++ PimpMyParrot.sh
@@ -120,16 +120,9 @@ wget https://upload.wikimedia.org/wikipedia/commons/0/0f/Grosser_Panda.JPG -P ~/
 
 # Install nvim
 echo -e "${GREEN}[+] Install nvim${NC}"
-sudo apt-get install neovim
-sudo apt-get install python3-neovim
-# CUSTOM_NVIM_PATH=/usr/local/bin/nvim.appimage
-# # Set the above with the correct path, then run the rest of the commands:
-# set -u
-# sudo update-alternatives --install /usr/bin/ex ex "${CUSTOM_NVIM_PATH}" 110
-# sudo update-alternatives --install /usr/bin/vi vi "${CUSTOM_NVIM_PATH}" 110
-# sudo update-alternatives --install /usr/bin/view view "${CUSTOM_NVIM_PATH}" 110
-# sudo update-alternatives --install /usr/bin/vim vim "${CUSTOM_NVIM_PATH}" 110
-# sudo update-alternatives --install /usr/bin/vimdiff vimdiff "${CUSTOM_NVIM_PATH}" 110
+apt-get install neovim
+apt-get install python3-neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 wget https://raw.githubusercontent.com/zhou-efr/PimpMyParrot/main/init.vim -P ~/.config/nvim
 
 # Install ZSH
