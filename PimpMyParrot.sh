c9e6884bbd84d93cbda290f568c374a635c1cf5d
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2023-08-09 11:03:15 +0200

    gogh fix

--- PimpMyParrot.sh
+++ PimpMyParrot.sh
@@ -233,9 +233,7 @@ echo_green "Install Gogh"
 mkdir -p "$HOME/src"
 cd "$HOME/src"
 git clone https://github.com/Gogh-Co/Gogh.git gogh
-cd gogh
-cd installs
-./bluloco-zsh-light.sh
+~/src/gogh/installs/bluloco-zsh-light.sh
 cd /tmp
 
 # Install mitm6
