17bad3a81462beacccbe91d141c78409f4150977
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2023-08-09 12:06:12 +0200

    fix end text

--- PimpMyParrot.sh
+++ PimpMyParrot.sh
@@ -272,7 +272,7 @@ sudo apt-get install aptitude -y
 
 echo_green "All automatic installation done !"
 echo_yellow "however, you still need to run manually :"
-echo_yellow " - [chsh -s $(which zsh)] to change your default shell"
-echo_yellow " - [sudo aptitude install freerdp2-x11] to install freerdp2-x11, note that you'll have to answer no then two times yes"
+echo_yellow ' - [chsh -s $(which zsh)] to change your default shell'
+echo_yellow ' - [sudo aptitude install freerdp2-x11] to install freerdp2-x11, note that you'll have to answer no then two times yes'
 
 /bin/zsh
